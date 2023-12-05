ea2e8350df10b9dd9a58d590022835d045a08980
QuentinF10 <qf10pro@gmail.com>
2023-12-04 22:40:18 -0700

    final product

--- src/VoteVista.java
+++ src/VoteVista.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 public class VoteVista {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-
             new VoteVistaUI();
         });
 
